ea22fdcb7756d11186c77adad66e151f7ab67260
ValentinPastre <136096741+ValentinPastre@users.noreply.github.com>
2025-09-02 14:30:28 -0300

    changed key bindings for neo-tree

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -12,7 +12,7 @@ return {
   lazy = false,
   keys = {
     { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
-    { '<leader>g', ':Neotree toggle<CR>', desc = 'NeoTree toggle', silent = true },
+    { '<leader>b', ':Neotree toggle<CR>', desc = 'NeoTree toggle', silent = true },
   },
   opts = {
     filesystem = {
